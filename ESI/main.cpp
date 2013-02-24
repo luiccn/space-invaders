fd80af3a30e75ba951698efa7f83ea2b862e2b7c
Luiz Carlos Ciafrino Neto <luiccn@users.noreply.github.com>
2013-02-24 05:17:56 +0000

    organization!

---
+++ ESI/main.cpp
@@ -0,0 +1,277 @@
+/* 
+ * File:   main.cpp
+ * Author: priscila
+ *
+ * Created on February 21, 2013, 01:22 AM
+ */
+
+#include <cstdlib>
+#include <allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/keyboard.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+ALLEGRO_DISPLAY *janela = NULL;
+ALLEGRO_FONT *fonteMenu = NULL;
+ALLEGRO_FONT *fonte2 = NULL;
+ALLEGRO_FONT *choice = NULL;
+ALLEGRO_BITMAP *imagem = NULL;
+ALLEGRO_SAMPLE *music = NULL;
+
+bool done = false;
+
+
+const int X = 1024;
+const int Y = 768;
+
+int draw_menu(){
+    imagem = al_load_bitmap("images/display.png");
+    
+    if (!imagem)
+        cout << "error" << endl;
+    
+    char *menuOptions[4] = { "Start Game", "Credits", "About this game", "Quit" }; 
+     // Carregando o arquivo de fonte
+    fonteMenu = al_load_font("fonts/Squareo.ttf", 39, 0);
+    if (!fonteMenu)
+    {
+        al_destroy_display(janela);
+        fprintf(stderr, "Falha ao carregar fonte.\n");
+        return -1;
+    }
+    //plano de fundo
+    al_draw_bitmap(imagem, 0, 0, 0);
+
+    //menu options
+      // Texto alinhado à esquerda
+    al_draw_text(fonte2, al_map_rgb(0, 235, 0), 10, 10, ALLEGRO_ALIGN_LEFT, "J");
+    // Texto alinhado à direita
+    al_draw_text(fonte2, al_map_rgb(0, 235, 0), X - 10, 50, ALLEGRO_ALIGN_RIGHT, "N");
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 400, ALLEGRO_ALIGN_LEFT, menuOptions[0]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 450, ALLEGRO_ALIGN_LEFT, menuOptions[1]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 500, ALLEGRO_ALIGN_LEFT, menuOptions[2]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 550, ALLEGRO_ALIGN_LEFT, menuOptions[3]);
+    
+    choice = al_load_font("fonts/spacefont.ttf", 36, 0);
+    
+
+    
+    al_flip_display();
+    
+}
+
+
+void rotate_sbvb(){
+    
+    ALLEGRO_BITMAP *sbvb;
+    
+    sbvb = al_load_bitmap("images/sbvb.png");
+    imagem = al_load_bitmap("images/display.png");
+    
+    
+    if (!sbvb)
+        cout << "error" << endl;
+    if (!imagem)
+        cout << "error" << endl;
+    
+     float angle=0.5;
+    for(int i=0; i< 50; i++){
+        
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+        al_draw_bitmap(imagem, 0, 0, 0);
+        al_draw_scaled_rotated_bitmap(sbvb,321/2.0,315/2.0,1024/2,768/2,1.5,1.5,angle,0);
+        al_flip_display();
+        angle+=0.1;
+    }
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+        al_draw_bitmap(imagem, 0, 0, 0);
+
+}
+        
+
+
+ int load_menu(){
+     int pos = 1;
+     char *menuOptions[4] = { "Start Game", "Credits", "About this game", "Quit" }; 
+     // Carregando o arquivo de fonte
+    fonteMenu = al_load_font("fonts/Squareo.ttf", 39, 0);
+    if (!fonteMenu)
+    {
+        al_destroy_display(janela);
+        fprintf(stderr, "Falha ao carregar fonte.\n");
+        return -1;
+    }
+     
+    //menu options
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 400, ALLEGRO_ALIGN_LEFT, menuOptions[0]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 450, ALLEGRO_ALIGN_LEFT, menuOptions[1]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 500, ALLEGRO_ALIGN_LEFT, menuOptions[2]);
+    al_draw_text(fonteMenu, al_map_rgb(50, 200, 115), X / 3, 550, ALLEGRO_ALIGN_LEFT, menuOptions[3]);
+    
+    choice = al_load_font("fonts/spacefont.ttf", 36, 0);
+    
+    al_draw_text(choice, al_map_rgb(255, 255, 255), X / 3 - 40, 400, ALLEGRO_ALIGN_LEFT, "Q");
+        
+    al_flip_display();
+    
+    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    
+    
+    while(!done){
+        ALLEGRO_EVENT events;
+        al_wait_for_event(event_queue, &events);
+        if(events.type == ALLEGRO_EVENT_KEY_DOWN){
+            switch(events.keyboard.keycode){
+                case ALLEGRO_KEY_DOWN:
+                    draw_menu();
+                    cout << pos << endl;
+                    pos+=1;
+                    cout << pos << endl;
+                    if (pos>4)
+                        pos = 1;
+                    al_draw_text(choice, al_map_rgb(255, 255, 255), X / 3 - 40, 350+50*pos, ALLEGRO_ALIGN_LEFT, "Q");
+                    al_flip_display();
+                    break;
+                case ALLEGRO_KEY_UP:
+                    draw_menu();
+                    pos-=1;
+                    if (pos<1)
+                        pos = 4;
+                    al_draw_text(choice, al_map_rgb(255, 255, 255), X / 3 - 40, 350+50*pos, ALLEGRO_ALIGN_LEFT, "Q");
+                    al_flip_display();
+                    break;
+                case ALLEGRO_KEY_ESCAPE:
+                    done = true;
+                    break;
+                case ALLEGRO_KEY_A: //ainda em testes
+                    rotate_sbvb();
+                    al_flip_display();
+                    draw_menu();
+                    break;
+                    
+            }
+        }
+            
+    }    
+    
+    return 0;
+}
+ 
+ int install_periferics(){
+     // if we get trouble the function returns 1 and close the app
+    if ( !al_install_mouse() ){
+        return 1; 
+    }
+    
+    if ( !al_install_keyboard() ) {
+        return 1;
+    }
+     
+ }
+ 
+ void load_music(){
+    al_install_audio();
+    al_init_acodec_addon();
+    music = al_load_sample("sound/intro.ogg");
+    al_play_sample(music, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_LOOP,NULL);   
+ }
+ 
+int main(void)
+{
+   // al_set_new_display_flags(ALLEGRO_FULLSCREEN);
+     
+    // the library
+    if (!al_init())
+    {
+        fprintf(stderr, "Falha ao inicializar a Allegro.\n");
+        return -1;
+    }
+      
+    // Inicialização do add-on para uso de fontes
+    al_init_font_addon();
+    
+    if (!al_install_audio())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_ttf.\n");
+        return -1;
+    }
+    // Inicialização dos recursos que serão utilizados
+    if (!al_init_ttf_addon())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_ttf.\n");
+        return -1;
+    }
+ 
+    if (!al_reserve_samples(1)){
+      fprintf(stderr, "failed to reserve samples!\n");
+      return -1;
+    }
+       
+    // Criação da nossa janela        
+    janela = al_create_display(X, Y);
+    if (!janela)
+    {
+        fprintf(stderr, "Falha ao criar janela.\n");
+        return -1;
+    }
+    
+    
+    // Carregando outro arquivo de fonte
+    fonte2 = al_load_font("fonts/Space Shop.ttf", 72, 0);
+    if (!fonte2)
+    {
+        al_destroy_display(janela);
+        fprintf(stderr, "Falha ao carregar fonte 2.\n");
+        return -1;
+    }
+    
+    load_music();
+    
+    // Preenchemos a tela com uma cor
+    al_clear_to_color(al_map_rgb(255, 220, 220));
+    
+    if (!al_init_image_addon()){
+      fprintf(stderr, "failed to reserve samples!\n");
+      return -1;
+    }
+     
+    //load images
+    imagem = al_load_bitmap("images/display.png");
+    // draw the image on screen
+    al_draw_bitmap(imagem, 0, 0, 0);
+    
+    // Texto alinhado à esquerda
+    al_draw_text(fonte2, al_map_rgb(0, 235, 0), 10, 10, ALLEGRO_ALIGN_LEFT, "J");
+ 
+    // Texto alinhado à direita
+    al_draw_text(fonte2, al_map_rgb(0, 235, 0), X - 10, 50, ALLEGRO_ALIGN_RIGHT, "N");
+    
+    install_periferics();
+        
+  
+    load_menu();
+    
+
+    
+    al_flip_display();
+     
+    // Tela é exibida por 8 segundos
+  //  al_rest(14.0);
+ 
+    // Desalocação da fonte e da janela
+    al_destroy_font(fonteMenu);
+    al_destroy_display(janela);
+    allegro_exit();
+ 
+    return 0;
+}
+
---
+++ ESI/nbproject/Makefile-Debug.mk
@@ -0,0 +1,84 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-L/usr/lib -Wl,-Bsymbolic-functions -lalleg -lm -lXxf86vm -lXcursor -lXpm -lXext -lX11 -lpthread -ldl `pkg-config --cflags --libs allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`  
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/esi
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/esi: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} `pkg-config --cflags --libs allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0` -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/esi  ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g `pkg-config --cflags --cflags allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`    -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/esi
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
